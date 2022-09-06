c44ff1a439503805a1b9897027bb0c099b425743
febianareswara <89761118+febianareswara@users.noreply.github.com>
2022-09-06 20:14:48 +0700

    Add files via upload

---
+++ MyProject2/src/myproject2/MyMainClass2.java
@@ -0,0 +1,15 @@
+
+package myproject2;
+public class MyMainClass2 {
+    public static void main(String[] args) {
+        System.out.println("FEBIA");
+        System.out.println("FFFFFFF  EEEEEEE  BBBBBBB");
+        System.out.println("FF       EE       BB   BBB");
+        System.out.println("FF       EE       BB  BBB");
+        System.out.println("FFFFFF   EEEEEEE  BBBBBB");
+        System.out.println("FF       EE       BB  BBB");
+        System.out.println("FF       EE       BB   BBB"); 
+        System.out.println("FF       EEEEEEE  BBBBBBB");   
+    }   
+    
+}
