c0658cb53de5cf09e457e5148f323395ec079046
febianareswara <89761118+febianareswara@users.noreply.github.com>
2022-09-06 20:19:21 +0700

    Add files via upload

---
+++ ProjectBangunDatar/src/projectbangundatar/ProjectBangunDatar.java
@@ -0,0 +1,35 @@
+package projectbangundatar;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class ProjectBangunDatar {
+    public static void main(String[] args) {
+        PersegiPanjang C = new PersegiPanjang();
+        C.panjang = 17;
+        C.lebar = 8;
+        
+        C.hitungLuas();
+        C.hitungKelliling();
+        
+        PersegiPanjang D = new PersegiPanjang();
+        D.panjang = 26;
+        D.lebar = 14;
+        
+        D.hitungLuas();
+        D.hitungKelliling();
+        
+        Lingkaran L1 = new Lingkaran();
+        L1.r = 6;
+        
+        L1.hitungLuasLingkaran();
+        L1.hitungKelilingLingkaran();
+                
+        Lingkaran L2 = new Lingkaran();
+        L2.r = 12;
+        
+        L2.hitungLuasLingkaran();
+        L2.hitungKelilingLingkaran();
+    }
+}
